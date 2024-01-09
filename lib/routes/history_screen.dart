91787198ea54b044f86e0d671e71d280169a3c9a
TallestLegacy <tallestlegacy@github.com>
2024-01-09 23:41:10 +0300

    realtime data baby!!!

--- lib/routes/history_screen.dart
+++ lib/routes/history_screen.dart
@@ -1,13 +1,43 @@
+import 'package:classifier/types.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../constants.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final myPredictionsQuery = FirebaseFirestore.instance
+        .collection(predictionsCollection)
+        .where("userId", isEqualTo: FirebaseAuth.instance.currentUser!.uid);
+
     return Scaffold(
       appBar: AppBar(title: const Text("History")),
-      body: const Text("History"),
+      body: FirestoreListView<Map<String, dynamic>>(
+        query: myPredictionsQuery,
+        loadingBuilder: (context) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        itemBuilder: (context, snapshot) {
+          var data = snapshot.data();
+          var prediction = StoredPrediction.fromJson(data);
+
+          return Column(
+            children: [
+              ListTile(
+                title: Text(prediction.time.toDate().toString()),
+                subtitle: Text(
+                    "${prediction.confidence} chance of ${prediction.prediction}"),
+              ),
+              const Divider(),
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/types.dart
+++ lib/types.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class PredictionResponse {
   final String confidence;
   final String prediction;
@@ -11,6 +13,29 @@ class PredictionResponse {
     return PredictionResponse(
       confidence: json['confidence'],
       prediction: json['predicted_class'],
+    );
+  }
+}
+
+class StoredPrediction {
+  final String prediction;
+  final String confidence;
+  final String userId;
+  final Timestamp time;
+
+  StoredPrediction({
+    required this.prediction,
+    required this.confidence,
+    required this.userId,
+    required this.time,
+  });
+
+  factory StoredPrediction.fromJson(Map<String, dynamic> json) {
+    return StoredPrediction(
+      prediction: json['prediction'],
+      confidence: json['confidence'],
+      userId: json['userId'],
+      time: json['time'],
     );
   }
 }
